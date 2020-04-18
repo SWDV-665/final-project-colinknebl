ca1a5e9feaca93210d67450c65c6fe1fb7df2d88
Colin Knebl <colinknebl@outlook.com>
2020-04-18 16:28:43 -0400

    update photo service

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -22,38 +22,27 @@ export class PhotoService {
 		source: CameraSource.Camera,
 		quality: 100,
 	};
-	private qr: {
-		decode(path: string): IScan;
-		callback(error: Error, result: IScan): void;
-	};
 	private _dal: DalService = new DalService();
 
-	constructor() {
-		this._initQrCodeReader();
+	constructor() {}
+
+	public async addNewToGallery(): Promise<void> {
+		return new Promise(async (resolve, reject) => {
+			const capturedPhoto = await Camera.getPhoto(
+				PhotoService.CameraOptions
+			);
+
+			const qr = new QrCode();
+			qr.callback = (error, scan) => {
+				if (error) {
+					console.log(error);
+					return reject(error);
+				}
+				this._dal.save(scan.result);
+				return resolve();
+			};
+
+			qr.decode(capturedPhoto.webPath);
+		});
 	}
-
-	public async addNewToGallery() {
-		const capturedPhoto = await Camera.getPhoto(PhotoService.CameraOptions);
-
-		this.qr.decode(capturedPhoto.webPath);
-	}
-
-	private _initQrCodeReader(): void {
-		this.qr = new QrCode();
-		this.qr.callback = (error, scan) => {
-			if (error) {
-				console.log(error);
-				return;
-			}
-			// image decoded
-			console.log('scan', scan);
-			this._dal.save('my scan', scan.result);
-			return scan;
-		};
-	}
-}
-
-interface IScan {
-	result: string;
-	points: any[];
 }
