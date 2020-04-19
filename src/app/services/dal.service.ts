1000a5e8343cada7427dc1adafb7e41d35ef6e4e
Colin Knebl <colinknebl@outlook.com>
2020-04-19 11:19:10 -0400

    add observable to dal service

--- src/app/services/dal.service.ts
+++ src/app/services/dal.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class DalService {
+	private static _Instance: DalService;
 	static key = 'scans';
+	public dataChanged$: Observable<IScan[]>;
+	private _dataChangedSubject: Subject<IScan[]>;
 
-	constructor() {}
+	constructor() {
+		if (DalService._Instance) {
+			return DalService._Instance;
+		}
+		DalService._Instance = this;
+		this._dataChangedSubject = new Subject();
+		this.dataChanged$ = this._dataChangedSubject.asObservable();
+	}
+
+	public subscribe(
+		next?: (value: IScan[]) => void,
+		error?: (error: any) => void,
+		complete?: () => void
+	) {
+		return this.dataChanged$.subscribe(next, error, complete);
+	}
 
 	public save(data: string): number {
 		const scans = this.fetch();
@@ -15,13 +34,18 @@ export class DalService {
 			data,
 		});
 		this._commit(scans);
+		this._dataChangedSubject.next(scans);
 		return numOfScans;
 	}
 
 	public fetch(): IScan[] {
 		let scans = localStorage.getItem(DalService.key);
 		if (!scans) return [];
-		return JSON.parse(scans);
+		const parsedScans = JSON.parse(scans);
+		return parsedScans.map((scan) => ({
+			data: scan.data,
+			date: new Date(scan.date),
+		}));
 	}
 
 	private _commit(scans: IScan[]): void {
@@ -29,7 +53,7 @@ export class DalService {
 	}
 }
 
-interface IScan {
+export interface IScan {
 	date: Date;
 	data: string;
 }
